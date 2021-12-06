91fbfa0585a2d1a260854ed379b05a51c792ed7c
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-12-06 13:11:14 -0500

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -2,26 +2,32 @@ import React, { useReducer } from "react";
 import reducer, { initialState} from './reducers/calcReducer';
 import "./styles.css";
 
+
+const add = () => {
+  return({type:"ADD", payload:2})
+}
+
+
 let currentState = initialState;
 console.log(currentState);
 
-currentState = reducer(currentState, {type:"ADD", payload:2});
+currentState = reducer(currentState, add());
 console.log(currentState);
 
-currentState = reducer(currentState, {type:"ADD", payload:10});
-console.log(currentState);
+// currentState = reducer(currentState, {type:"ADD", payload:10});
+// console.log(currentState);
 
-currentState = reducer(currentState, {type:"ADD", payload:3});
-console.log(currentState);
+// currentState = reducer(currentState, {type:"ADD", payload:3});
+// console.log(currentState);
 
-currentState = reducer(currentState, {type:"SUBTRACT", payload:6});
-console.log(currentState);
+// currentState = reducer(currentState, {type:"SUBTRACT", payload:6});
+// console.log(currentState);
 
-currentState = reducer(currentState, {type:"ADD_TO_MEMORY"});
-console.log(currentState);
+// currentState = reducer(currentState, {type:"ADD_TO_MEMORY"});
+// console.log(currentState);
 
-currentState = reducer(currentState, {type:"CLEAR"});
-console.log(currentState);
+// currentState = reducer(currentState, {type:"CLEAR"});
+// console.log(currentState);
 
 
 export default function App() {
