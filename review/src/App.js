10e6104d0725f025ec4dbf32f1a7c08d85bd4ab9
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-12-06 13:22:46 -0500

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -27,7 +27,7 @@ export default function App() {
   const [state, dispatch ] = useReducer(reducer, initialState);
   
   const handleAdd = () => {
-    
+    state = reducer(state, add(5));
   }
 
   return (
