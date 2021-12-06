f57ff4b0a3b990e7b50daf595c1f85458189c5d8
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-12-06 14:01:13 -0500

    lecture work-in-progress

--- followAlong/src/actions/index.js
+++ followAlong/src/actions/index.js
@@ -10,3 +10,8 @@ export const CHANGE_NEW_TITLE = "CHANGE_NEW_TITLE";
 export const changeNewTitle = (newTitle)=> {
     return({type: CHANGE_NEW_TITLE, payload:newTitle});
 }
+
+export const EDIT_TITLE = "EDIT_TITLE";
+export const editTitle = () => {
+    return({type: EDIT_TITLE});
+}
