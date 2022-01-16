2533fa7b9adad4de229fc299801136875870c9e7
Jeremiah Candelaria <jeremiahcandelaria@gmail.com>
2022-01-15 20:53:33 -0800

    complete mvp tasks

---
+++ followAlong/src/reducers/index.js
@@ -0,0 +1,41 @@
+import { TOGGLE_EDIT, SET_TITLE_TEXT, SAVE_INPUT } from "../actions";
+
+export const initialState = {
+    title: 'Hello earthlings',
+    editing: false,
+    newTitleText: ''
+}
+
+const reducer = (state, action) => {
+    // SWITCH STATEMENT:
+    switch(action.type) {
+        case(TOGGLE_EDIT):
+            return({...state, editing: !state.editing});
+        case(SET_TITLE_TEXT):
+            return({...state, newTitleText: action.payload});
+        case(SAVE_INPUT):
+            return({
+                ...state,
+                editing: false,
+                title: state.newTitleText
+            });
+        default:
+            return state;
+    }
+
+    // IF / ELSE STATEMENT:
+    // if(action.type === TOGGLE_EDIT) {
+    //     return({ ...state, editing: !state.editing })
+    // } else if (action.type === SET_TITLE_TEXT) {
+    //     return({ ...state, newTitleText: action.payload });
+    // } else if (action.type === SAVE_INPUT) {
+    //     return({
+    //         ...state,
+    //         editing: false,
+    //         title: state.newTitleText
+    //     });
+    // }
+    // return state;
+}
+
+export default reducer;
